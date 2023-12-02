015377974475f81e3ac87050140e9c71a1a4bf64
Postigic <143212308+Postigic@users.noreply.github.com>
2023-12-03 01:05:46 +0800

    Update error_checking_methods.py

--- random or unmarked/error_checking_methods.py
+++ random or unmarked/error_checking_methods.py
@@ -9,7 +9,7 @@ NOISE_PROBABILITY = 0.5
 # USER INPUT
 
 
-def bin_str_input():
+def binary_string_input():
     while True:
         user_input = input(
             "Enter a valid binary string or generate a random one: ").lower()
@@ -17,7 +17,7 @@ def bin_str_input():
             while True:
                 length = input("Enter desired binary string length: ")
                 if length.isdigit():
-                    return gen_rand_bin_str(int(length))
+                    return generate_random_binary_string(int(length))
                 else:
                     print("Invalid input. Length must be a number.")
         elif user_input.isdigit() and set(user_input).issubset({"0", "1"}):
@@ -29,8 +29,8 @@ def bin_str_input():
 def error_check_choice():
     while True:
         check_type = input(
-            "Select an error-checking method (parity, checksum, fcs, crc): ")
-        if check_type.lower() not in {"parity", "checksum", "fcs", "crc"}:
+            "Select an error-checking method (parity, checksum): ")
+        if check_type.lower() not in {"parity", "checksum"}:
             print("Invalid error-checking method.")
             continue
         return check_type
@@ -38,7 +38,7 @@ def error_check_choice():
 # PARITY CHECK
 
 
-def get_parity_choice():
+def get_parity():
     while True:
         parity_choice = input(
             "Choose between Even or Odd parity check (e, o): ")
@@ -48,78 +48,74 @@ def get_parity_choice():
         return parity_choice
 
 
-def add_parity_bit(data, parity_choice):
-    if parity_choice == "e":
-        parity_bit = "0" if data.count("1") % 2 == 0 else "1"
+def add_parity_bit(binary_data, parity):
+    count_of_ones = binary_data.count("1")
+    if parity == "e":
+        parity_bit = "0" if count_of_ones % 2 == 0 else "1"
     else:
-        parity_bit = "1" if data.count("1") % 2 == 0 else "0"
-    return parity_bit + data
+        parity_bit = "1" if count_of_ones % 2 == 0 else "0"
+    return parity_bit + binary_data
 
 
-def parity_check_data(data, parity_choice):
-    total = sum(int(bit) for bit in data)
-    if (total % 2 == 0 and parity_choice == 'e') or (total % 2 == 1 and parity_choice == 'o'):
-        return "No corruption."
+def parity_check_data(binary_data, parity):
+    total_sum = sum(int(bit) for bit in binary_data)
+    if (total_sum % 2 == 0 and parity == 'e') or (total_sum % 2 == 1 and parity == 'o'):
+        return "No corruption detected."
     return "Corruption detected."
 
 # CHECKSUM
 
 
-def calculate_checksum(data):
-    checksum = int(data, 2)
+def calculate_checksum(binary_data):
+    checksum = int(binary_data, 2)
     if checksum > 255:
         checksum = checksum % 256
-    return f"{data}, {str(checksum)}"
+    return f"{binary_data}, {str(checksum)}"
 
 
-def checksum_check_data(data):
-    split_data = data.split(", ")
-    data, checksum = split_data
-    recalculated_checksum = calculate_checksum(data)
-    if int(checksum) == recalculated_checksum:
+def checksum_check_data(binary_data):
+    received_data, checksum_str = binary_data.split(", ")
+    recalculated_checksum = calculate_checksum(received_data)
+    if int(checksum_str) == int(recalculated_checksum.split(", ")[1]):
         return "No corruption."
     return "Corruption detected."
 
-# FRAME CHECK SEQUENCE
-
-# CYCLIC REDUNDANCY CHECK
 
 # MAIN
 
 
-def gen_rand_bin_str(length):
+def generate_random_binary_string(length):
     return ''.join(random.choice('01') for _ in range(length))
 
 
-def add_noise(data):
+def add_noise(binary_data):
     probability = random.random()
     if probability < NOISE_PROBABILITY:
-        index = random.randint(0, len(data) - 1)
-        data_list = list(data)
-        data_list[index] = "0" if data_list[index] == "1" else "1"
-        data = "".join(data_list)
-    return data
+        index = random.randint(0, len(binary_data) - 1)
+        binary_data = binary_data[:index] + ("0" if binary_data[index] ==
+                                             "1" else "1") + binary_data[index+1:]
+    return binary_data
 
 
 if __name__ == "__main__":
-    original_data = bin_str_input()
-    check_type = error_check_choice()
+    binary_string = binary_string_input()
+    error_check_type = error_check_choice()
 
-    if check_type == "parity":
-        parity_choice = get_parity_choice()
-        parity_data = add_parity_bit(original_data, parity_choice)
+    if error_check_type == "parity":
+        parity_choice = get_parity()
+        parity_data = add_parity_bit(binary_string, parity_choice)
         received_data = add_noise(parity_data)
         # ------------------------------------------------------ #
-        print(f"Original data: {original_data}")
+        print(f"Original data: {binary_string}")
         print(f"Data with parity bit: {parity_data}")
         print(f"Received data: {received_data}")
         print(parity_check_data(received_data, parity_choice))
 
-    elif check_type == "checksum":
-        checksum_data = calculate_checksum(original_data)
+    elif error_check_type == "checksum":
+        checksum_data = calculate_checksum(binary_string)
         received_data = add_noise(checksum_data)
         # ------------------------------------------------------ #
-        print(f"Original data: {original_data}")
+        print(f"Original data: {binary_string}")
         print(f"Data with checksum: {checksum_data}")
         print(f"Received data: {received_data}")
         print(checksum_check_data(checksum_data))
