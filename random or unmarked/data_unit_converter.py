31c0aeeed6f52be37b6bbc388dc80f3c819a86fc
Postigic <143212308+Postigic@users.noreply.github.com>
2024-01-05 15:12:24 +0800

    Update and rename byte_converter.py to data_unit_converter.py

--- random or unmarked/data_unit_converter.py
+++ random or unmarked/data_unit_converter.py
@@ -40,19 +40,30 @@ def calculate_bytes(data_size, initial_unit, conversion_unit, conversion_factor)
     return data_size * initial_bytes / conversion_bytes
 
 
+def calculate_bits(data_size, unit, conversion_factor):
+    bytes_size = calculate_bytes(data_size, unit, "B", conversion_factor)
+    return bytes_size * 8
+
+
 def main():
-    data_size = get_float_input("Enter data size: ")
-    valid_units = ["B", "KB", "MB", "GB", "TB"]
-    initial_unit = get_unit(
-        "Enter data unit (B, KB, MB, GB, TB): ", valid_units)
-    conversion_unit = get_unit(
-        "Enter conversion unit (B, KB, MB, GB, TB): ", valid_units)
-    valid_factors = [1000, 1024]
-    conversion_factor = get_conversion_factor(
-        "Enter conversion factor (1000 or 1024): ", valid_factors)
-    result = calculate_bytes(data_size, initial_unit,
-                             conversion_unit, conversion_factor)
-    print(f"Result: {result:.6f} {conversion_unit}")
+    while True:
+        data_size = get_float_input("Enter data size: ")
+        valid_units = ["B", "KB", "MB", "GB", "TB"]
+        initial_unit = get_unit(
+            "Enter data unit (B, KB, MB, GB, TB): ", valid_units)
+        conversion_unit = get_unit(
+            "Enter conversion unit (B, KB, MB, GB, TB): ", valid_units)
+        valid_factors = [1000, 1024]
+        conversion_factor = get_conversion_factor(
+            "Enter conversion factor (1000 or 1024): ", valid_factors)
+
+        bytes_result = calculate_bytes(
+            data_size, initial_unit, conversion_unit, conversion_factor)
+        bits_result = calculate_bits(
+            data_size, initial_unit, conversion_factor)
+
+        print(f"Bytes Result: {bytes_result:.6f} {conversion_unit}")
+        print(f"Bits Result: {bits_result:.6f} bits")
 
 
 if __name__ == "__main__":
