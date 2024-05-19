9e79ab0aea4ae9b49455bb59e7e74ecd939d816b
Postigic <143212308+Postigic@users.noreply.github.com>
2024-05-19 16:06:40 +0800

    Add files via upload

---
+++ random_or_unmarked/monoalphabetic_substitution_cipher.py
@@ -0,0 +1,59 @@
+import string
+alphabet = string.ascii_lowercase
+key = "qwertyuiopasdfghjklzxcvbnm"
+
+
+def create_maps(alphabet, key):
+    encryption_map = {}
+    decryption_map = {}
+    for i in range(len(alphabet)):
+        encryption_map[alphabet[i]] = key[i]
+        decryption_map[key[i]] = alphabet[i]
+    return encryption_map, decryption_map
+
+
+def cipher(text, mapping):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                result += mapping[char.lower()].upper()
+            else:
+                result += mapping[char]
+        else:
+            result += char
+
+    return result
+
+
+def frequency_analysis(text):
+    frequencies = {letter: 0 for letter in string.ascii_lowercase}
+
+    for char in text:
+        char_lower = char.lower()
+        if char_lower in string.ascii_lowercase:
+            frequencies[char_lower] += 1
+
+    frequencies = dict(
+        sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
+    return frequencies
+
+
+if __name__ == "__main__":
+    plain_text = "Hello World! The quick brown fox jumps over the lazy dog."
+    encrypted_text = "Itssg Vgksr! Zit jxoea wkgvf ygb pxdhl gctk zit sqmn rgu."
+
+    encryption_map, decryption_map = create_maps(alphabet, key)
+
+    encrypted_result = cipher(plain_text, encryption_map)
+    encrypted_analysis = frequency_analysis(encrypted_result)
+    decrypted_analysis = frequency_analysis(plain_text)
+
+    print(f"Original Text:           {plain_text}")
+    print(f"Encrypted Text:          {encrypted_result}\n")
+
+    print("Frequency Analysis:")
+    for encrypted_freq, decrypted_freq in zip(encrypted_analysis.items(), decrypted_analysis.items()):
+        print(
+            f"{encrypted_freq[0]}: {encrypted_freq[1]} - {decrypted_freq[0]}: {decrypted_freq[1]}")
