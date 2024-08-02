4ef8d1b1f7019897ab6a1ab3dca4a4d7a7f9cabc
Postigic <143212308+Postigic@users.noreply.github.com>
2024-08-02 23:04:46 +0800

    Update data_management.py

--- Python/student_management_system/data_management.py
+++ Python/student_management_system/data_management.py
@@ -3,16 +3,22 @@ from student_class import Student
 
 
 def save_students(filename):
+    unique_student_ids = set()
+    unique_students = []
+
+    for student in Student.all_students:
+        student_id = student.generate_student_ID()
+        if student_id not in unique_student_ids:
+            unique_student_ids.add(student_id)
+            unique_students.append(student)
+
     with open(filename, "w") as file:
         json.dump([student.to_dict()
-                  for student in Student.all_students], file, indent=4)
+                  for student in unique_students], file, indent=4)
 
 
 def load_students(filename):
     with open(filename, "r") as file:
         student_data_list = json.load(file)
-
-    with open(filename, "w") as file:
-        file.write("[]")
 
     return [Student.from_dict(student_data) for student_data in student_data_list]
