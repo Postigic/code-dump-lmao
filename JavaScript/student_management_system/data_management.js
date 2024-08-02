84825ac4661bf5f0e1a70c1dc31850c9b329ce14
Postigic <143212308+Postigic@users.noreply.github.com>
2024-08-02 23:13:10 +0800

    Update data_management.js

--- JavaScript/student_management_system/data_management.js
+++ JavaScript/student_management_system/data_management.js
@@ -2,14 +2,23 @@ import { writeFileSync, readFileSync } from "fs";
 import Student from "./student_class.js";
 
 export function saveStudents(filename) {
-    const studentData = Student.allStudents.map((student) => student.toDict());
+    const uniqueStudentIds = new Set();
+    const uniqueStudents = [];
+
+    Student.allStudents.forEach((student) => {
+        const studentId = student.generateStudentID();
+        if (!uniqueStudentIds.has(studentId)) {
+            uniqueStudentIds.add(studentId);
+            uniqueStudents.push(student);
+        }
+    });
+
+    const studentData = uniqueStudents.map((student) => student.toDict());
     writeFileSync(filename, JSON.stringify(studentData, null, 4), "utf8");
 }
 
 export function loadStudents(filename) {
     const studentDataList = JSON.parse(readFileSync(filename, "utf8"));
-
-    writeFileSync(filename, "[]", "utf8");
 
     return studentDataList.map((studentData) => Student.fromDict(studentData));
 }
