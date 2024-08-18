460f4c2148d4a8d424fbbe5911e81a9f67edd31f
Postigic <143212308+Postigic@users.noreply.github.com>
2024-08-19 01:39:06 +0800

    updated again ALAFLA

--- Python/song_and_lyrics_player/main.py
+++ Python/song_and_lyrics_player/main.py
@@ -1,6 +1,5 @@
 import pygame
 import time
-import syllapy
 import random
 from youtube_transcript_api import YouTubeTranscriptApi
 from song_downloader import download_song_as_mp3
@@ -19,11 +18,10 @@ style = {
 
 
 def calculate_print_speed(text, duration):
-    total_syllables = sum(syllapy.count(word) for word in text.split())
-    if total_syllables <= 1:
+    total_length = len(text)
+    if total_length <= 1:
         return 0.03
-
-    return duration / (total_syllables * 10)
+    return duration / (total_length * 5)
 
 
 def slow_print(text, speed, colour=""):
@@ -35,58 +33,47 @@ def slow_print(text, speed, colour=""):
 def get_transcript(video_id):
     transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
-    transcript = transcript_list.find_transcript(
+    transcript = transcript_list.find_manually_created_transcript(
         ["en-US", "en-GB", "en-AU", "en"])
-    
+
     return transcript
 
 
-def print_lyrics(audio_file, transcript, colours, use_colours):
+def print_lyrics(audio_file, transcript, colours, volume=0.65, use_colours=True):
     pygame.mixer.init()
     pygame.mixer.music.load(audio_file)
-    total_duration = (pygame.mixer.Sound(audio_file)).get_length()
+    total_duration = pygame.mixer.Sound(audio_file).get_length()
     pygame.mixer.music.play()
+    pygame.mixer.music.set_volume(volume)
 
     lyrics = transcript.fetch()
     current_lyric_index = 0
-    iteration_counter = 0
-    running = True
 
-    while running:
-        current_lyric = lyrics[current_lyric_index]
-        start_time = current_lyric["start"]
-        duration = current_lyric["duration"]
+    while current_lyric_index < len(lyrics):
         current_time = pygame.mixer.music.get_pos() / 1000
+        current_lyric = lyrics[current_lyric_index]
 
-        if iteration_counter % 3 == 0:
-            colour = random.choice(
-                list(colours.values())) if use_colours else ""
-
-        if current_time >= start_time:
+        if current_time >= current_lyric["start"]:
+            colour = random.choice(list(
+                colours.values())) if use_colours and current_lyric_index % 3 == 0 else colour
             print_speed = calculate_print_speed(
-                current_lyric["text"], duration)
-            slow_print(
-                f"\n{current_lyric['text']}", print_speed, colour)
+                current_lyric["text"], current_lyric["duration"])
+            slow_print(f"\n{current_lyric['text']}", print_speed, colour)
             current_lyric_index += 1
-            iteration_counter += 1
 
-        if current_lyric_index >= len(lyrics):
-            time.sleep((total_duration + 1) - current_time)
-            running = False
+    time.sleep(max(0, total_duration - current_time))
 
 
 if __name__ == '__main__':
     current_dir = Path(__file__).parent
     output_path = current_dir / "mp3_files"
-    
-    video_id = "F3cXxqgbx9Y"
-    coloured_text = True
+
+    video_id = "uFRPeiAEO0M"
 
     colour_attributes = {k: v for k, v in style.items() if k != 'RESET'}
 
     audio_file_path = download_song_as_mp3(video_id, output_path)
     song_transcript = get_transcript(video_id)
-    print_lyrics(audio_file_path, song_transcript,
-                 colour_attributes, coloured_text)
+    print_lyrics(audio_file_path, song_transcript, colour_attributes)
 
     pygame.quit()
