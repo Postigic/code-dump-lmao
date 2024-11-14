82d60c0a5348d69ee68981f79ae6c967680834c2
Postigic <143212308+Postigic@users.noreply.github.com>
2024-11-14 09:48:03 +0800

    Update score_manager.py

--- Python/snake_game/score_manager.py
+++ Python/snake_game/score_manager.py
@@ -5,7 +5,6 @@ json_file_path = Path(__file__).parent / "high_score.json"
 
 
 def load_high_score():
-    """Load the high score from a JSON file."""
     try:
         with open(json_file_path, "r") as file:
             data = json.load(file)
@@ -15,6 +14,5 @@ def load_high_score():
 
 
 def save_high_score(high_score):
-    """Save the high score to a JSON file."""
     with open(json_file_path, "w") as file:
         json.dump({"high_score": high_score}, file)
