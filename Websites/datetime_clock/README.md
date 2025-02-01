64de61b8a89a5fde66ef73c031ab96bd4963592b
Postigic <wenkaiye13@gmail.com>
2025-02-02 00:12:49 +0800

    new project yatta!

---
+++ Websites/datetime_clock/README.md
@@ -0,0 +1,10 @@
+# datetime clock
+
+simple website of a datetime clock, just made it for fun lol
+</br>
+</br>
+made with... do i gotta write this again...? HTML, CSS, and JS
+</br>
+</br>
+image here:
+![image](./project_image/image.png)
---
+++ Websites/datetime_clock/script.js
@@ -0,0 +1,74 @@
+let timeFormat = "12-hour";
+let dateFormat = "long";
+let dayFormat = "long";
+
+function updateClock() {
+    const clockElement = document.getElementById("clock");
+    const timeElement = document.getElementById("date");
+
+    const now = new Date();
+
+    let hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    let meridiem = "";
+
+    if (timeFormat === "12-hour") {
+        meridiem = hours >= 12 ? "PM" : "AM";
+        hours = hours % 12 || 12;
+    }
+
+    const weekdayOption =
+        dayFormat === "none"
+            ? undefined
+            : dayFormat === "long"
+            ? "long"
+            : "short";
+
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    let dateString = now.toLocaleDateString("en-SG", {
+        weekday: weekdayOption,
+        day: dateFormat === "long" ? "numeric" : "2-digit",
+        month: dateFormat === "long" ? "long" : "2-digit",
+        year: "numeric",
+    });
+
+    clockElement.textContent = timeString;
+    timeElement.textContent = dateString;
+}
+
+updateClock();
+setInterval(updateClock, 100);
+
+const settingsButton = document.getElementById("settings-btn");
+const settingsMenu = document.getElementById("settings-menu");
+const closeSettingsButton = document.getElementById("close-settings");
+const timeFormatSelect = document.getElementById("time-format");
+const dateFormatSelect = document.getElementById("date-format");
+const showDaySelect = document.getElementById("show-day");
+
+settingsButton.addEventListener("click", () => {
+    settingsButton.style.opacity = "0";
+    settingsMenu.style.bottom = "0";
+});
+
+closeSettingsButton.addEventListener("click", () => {
+    settingsButton.style.opacity = "1";
+    settingsMenu.style.bottom = "-100%";
+});
+
+timeFormatSelect.addEventListener("change", () => {
+    timeFormat = timeFormatSelect.value;
+    updateClock();
+});
+
+dateFormatSelect.addEventListener("change", () => {
+    dateFormat = dateFormatSelect.value;
+    updateClock();
+});
+
+showDaySelect.addEventListener("change", () => {
+    dayFormat = showDaySelect.value;
+    updateClock();
+});
