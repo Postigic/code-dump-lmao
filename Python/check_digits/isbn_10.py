36f4385544a53a6c2595b8430c557c6509d37900
Postigic <wenkaiye13@gmail.com>
2025-02-25 22:08:55 +0800

    stupid brain doesn't understand how isbn works

--- Python/check_digits/isbn_10.py
+++ Python/check_digits/isbn_10.py
@@ -1,5 +1,5 @@
 def find_check_digit(isbn_chars):
-    c_isbn = isbn_chars.replace("-", "")
+    c_isbn = isbn_chars.replace('-', '')
 
     isbn_list = list(c_isbn)
 
@@ -13,13 +13,13 @@ def find_check_digit(isbn_chars):
     check_digit = (11 - (total % 11)) % 11
 
     if check_digit == 10:
-        return "X"
+        return 'X'
     else:
-        return check_digit
+        return str(check_digit)
     
 
 def validate_isbn(isbn):
-    c_isbn = isbn.replace("-", "")
+    c_isbn = isbn.replace('-', '')
 
     if len(c_isbn) != 10:
         return False
@@ -27,26 +27,18 @@ def validate_isbn(isbn):
     if not c_isbn[:9].isdigit():
         return False
     
-    isbn_list = list(c_isbn)
-
-    if not (isbn_list[9] == 'X' or isbn_list[9].isdigit()):
+    if not (c_isbn[9] == 'X' or c_isbn[9].isdigit()):
         return False
     
-    if isbn_list[9] == "X":
-        isbn_list[9] = "10"
+    check_digit = find_check_digit(c_isbn[:9])
 
-    total = 0
-    index = 0
-
-    for value in isbn_list:
-        total += int(value) * (10 - index)
-        index += 1
-        
-    return total % 11 == 0
+    return check_digit == c_isbn[9]
 
 
 print(find_check_digit("0-8044-2957"))
 print(find_check_digit("9971-5-0210"))
 
 print(validate_isbn("0-8044-2957-X"))
+print(validate_isbn("0-8044-2955-X"))
+print(validate_isbn("9971-5-0210-0"))
 print(validate_isbn("9971-5-0230-0"))
--- Python/check_digits/isbn_13.py
+++ Python/check_digits/isbn_13.py
@@ -10,25 +10,20 @@ def find_check_digit(isbn_chars):
         total += int(value) * (1 if index % 2 == 0 else 3)
         index += 1
 
-    return (10 - (total % 10)) % 10
+    return str((10 - (total % 10)) % 10)
 
 
 def validate_isbn(isbn):
     c_isbn = isbn.replace("-", "")
 
-    isbn_list = list(c_isbn)
+    check_digit = find_check_digit(c_isbn[:12])
 
-    total = 0
-    index = 0
-
-    for value in isbn_list:
-        total += int(value) * (1 if index % 2 == 0 else 3)
-        index += 1
-
-    return total % 10 == 0
+    return check_digit == c_isbn[12]
 
 
 print(find_check_digit("978-81-7525-766")) # 5
 
 print(validate_isbn("978-81-7525-766-5")) # True
 print(validate_isbn("978-81-7125-766-5")) # False
+print(validate_isbn("978-1-234-56789-7")) # True
+print(validate_isbn("979-1-234-56789-7")) # False
