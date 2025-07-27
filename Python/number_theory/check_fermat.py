8ca6c7f703a607bf09be9ece6c7d33c23e3028fb
Postigic <wenkaiye13@gmail.com>
2025-07-27 18:11:32 +0800

    more reorganising

--- Python/number_theory/check_fermat.py
+++ Python/number_theory/check_fermat.py
@@ -4,13 +4,11 @@ def check_fermat(a, b, c, n):
     else:
         print("No, that doesn't work.")
 
-
 def get_arguments():
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
     c = int(input("Enter c: "))
     n = int(input("Enter n: "))
     return check_fermat(a, b, c, n)
-
 
 get_arguments()
